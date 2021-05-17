8372c6919ec941e6916ff771a83fc76e0022514f
ooskdud <ooskdud@gmail.com>
2021-05-18 01:47:14 +0900

    이미지

--- page/models.py
+++ page/models.py
@@ -7,7 +7,8 @@ class Application(models.Model):
     age = models.CharField(max_length=100)
     introduce = models.TextField()
     pub_date = models.DateTimeField('date published', default = timezone.now)
-    image = models.ImageField(upload_to="post_img/", blank=True, null=True)
+    # image = models.ImageField(upload_to='post_img/', blank=True, null=True)
+    image = models.ImageField(upload_to="post_img/", default='null')
 
     def __str__(self):
         return self.name
